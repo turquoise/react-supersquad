99d8a9192dd0779b0f10a7770a8504f1b638b28e
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-22 17:48:36 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import logo from './logo.svg';
+//import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <h2>SuperSquad</h2>
+      
+      </div>
+    );
+  }
+}
+
+export default connect(null, { })(App);
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+//import registerServiceWorker from './registerServiceWorker';
+
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+// subscribe is an event listener that fires when the store is updated.
+store.subscribe( () => console.log('store ', store.getState()));
+
+ReactDOM.render(
+  <Provider store={store} >
+      <App />
+  </Provider>,
+  document.getElementById('root'));
+//registerServiceWorker();
